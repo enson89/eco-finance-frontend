af03f441485751ae0d724370aac36442edab4492
yowai-hito <55124974+yowai-hito@users.noreply.github.com>
2021-09-23 13:27:19 +0800

    Update ControlledTabs.js

--- src/ControlledTabs.js
+++ src/ControlledTabs.js
@@ -1,7 +1,7 @@
 import {Tabs, Tab} from 'react-bootstrap';
 import React, { useState} from 'react';
 import Portfolio from './Portfolio.js';
-import News from './News.js';
+import Transactions from './Transactions.js';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
@@ -9,8 +9,30 @@ import './App.css';
 
 function HorizontalTabs() {
     const [key, setKey] = useState('portfolio');
-  
+    const [data,setData] = useState('User')
+    
+    async function retrievename () {
+      return await fetch('https://eco-finance-backend.herokuapp.com/api/login', {
+        method: 'POST',
+        headers: {"Content-Type" :'application/json'},
+        body: localStorage.getItem('user')
+      })
+      .then((response) => response.json())
+    }
+    
+    async function greeting () {
+      let username = await retrievename();
+      console.log(username.name)
+      setData(username.name)
+    }
+    greeting()
+
     return (
+    <div>
+      <br/>
+        Hi, {data}
+      <br/>
+      <br/>
       <Tabs
         id="controlled-tab-example"
         activeKey={key}
@@ -20,10 +42,11 @@ function HorizontalTabs() {
         <Tab eventKey="portfolio" title="Portfolio">
           <Portfolio />
         </Tab>
-        <Tab eventKey="news" title="News" disabled>
-          <News /> 
+        <Tab eventKey="news" title="Transactions">
+          <Transactions /> 
         </Tab>
       </Tabs>
+    </div>
     );
   }
   
